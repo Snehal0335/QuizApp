6cc3b1aca1cbd334cd40b0d83ba0d678dc83c1de
Snehal0335 <snehalb.lendave@gmail.com>
2026-03-09 23:57:49 +0530

    Remove Flutter app code and add Ansible script

--- main.dart
+++ main.dart
@@ -1,69 +1,55 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'screens/start_page.dart';
-import 'screens/login_screen.dart';
-import 'screens/register_screen.dart';
-import 'screens/admin_dashboard.dart';
-import 'screens/student_dashboard.dart';
-import 'screens/quiz_list_screen.dart';
-import 'screens/createquizpage.dart';
-import 'screens/addquestionpage.dart';
+Prac 8
+Kali root terminal(first check ansible –version if not there )
+sudo apt update
+sudo apt –allow-insecure-repositories
+sudo apt install kali-archive-keyring –reinstall
+sudo abt install Ansible
+ansible –version
+nano install_nginx.yml
+--- 
+- name: Install and Start NGINX 
+  hosts: localhost 
+  become: yes 
+ 
+  tasks: 
+    - name: Install NGINX 
+      apt: 
+        name: nginx         
+state: present 
+ 
+    - name: Start Nginx service 
+      service: 
+        name: nginx         
+state: started 
+ 
+    - name: Create a file 
+      file:  
+        path: /home/kali/demo.txt         
+state: touch 
 
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
+run yml file
+ls
+ansible-playbook -i hosts install_ngnix.yml --ask-become-pass
+ls -i ~/demo.txt
+/var/www/html
+Ls
+Sudo rm index.html
 
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyDc5j5qDh_lfhjjLE-mJVUREQyVZXWkShk",
-        // from google-services.json
-        appId: "1:844715431752:android:d774e8605b69b887b5457d",
-        messagingSenderId: "844715431752",
-        projectId: "online-quiz-app-af545",
-      ),
-    );
-
-
-    runApp(const MyApp());
+Cicd github
+node {
+    stage('DEV') {
+        echo 'Cloning GitHub repository'
+        git branch: 'main',
+            url: 'https://github.com/Snehal0335/cc_ampify.git'
+    }
+    stage('QA') {
+        input 'QA DEPLOY?'
+    }
+    stage('DEPLOY TO PROD') {
+    bat 'xcopy "index.html" "C:\\Users\\Snehal\\Downloads\\apache-tomcat-11.0.18\\apache-tomcat-11.0.18\\webapps\\ROOT" /Y'
+    }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Online Quiz System',
-      theme: ThemeData(
-        primaryColor: const Color(0xFF5B4FFF),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFF5B4FFF),
-          secondary: const Color(0xFF5B4FFF),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF5B4FFF),
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          ),
-        ),
-      ),
-      home: const StartPage(),
-      routes: {
-        '/login': (context) => const LoginFormScreen(),
-        '/register': (context) => const RegisterScreen(),
-        '/adminDashboard': (context) => const AdminDashboard(adminId: "CURRENT_ADMIN_UID"),
-        '/studentDashboard': (context) => const StudentDashboard(),
-        '/quizList': (context) => const QuizListScreen(),
-        '/createquiz': (context) => const CreateQuizPage(),
-        '/addquestion': (context) => const AddQuestionPage(quizId: "PLACEHOLDER_QUIZ_ID"),
-
-      },
-    );
-  }
-}
